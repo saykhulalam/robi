34e2b7259142190244378a572fe04b4469db91b6
saykhul alam <156447267+saykhulalam@users.noreply.github.com>
2024-01-17 19:58:25 +0600

    responsive hoyni akhono

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$('.slide-tow').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<i class="fa-solid fa-chevron-left slidetow-left"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right slidetow-right"></i>',
+  });
